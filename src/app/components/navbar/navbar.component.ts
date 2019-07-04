c10ab1e4666bbac17a39ff43562845526280cb05
Anartz Mugika Ledo <mugan86@gmail.com>
2019-07-04 23:08:04 +0200

    Add header fixed with change color with scroll

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+// Declaramos las variables para jQuery
+declare var $: any;
 @Component({
   selector: 'blog-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 200) {
+          $('#mainNav').addClass('header-paint');
+      } else {
+          // remove the background property so it comes transparent again (defined in your css)
+         $('#mainNav').removeClass('header-paint');
+      }
+  });
+  }
 
   ngOnInit() {
   }
