a65aafe239fee4e95cc30c85c5a650f0e4e15628
Anartz Mugika Ledo <mugan86@gmail.com>
2019-08-25 10:10:23 +0200

    TSLINT clean

--- src/app/@core/components/header/header.constants.ts
+++ src/app/@core/components/header/header.constants.ts
@@ -1,49 +1,43 @@
 import { Header } from './header.interface';
 
-export const HOME: Header =
-{
+export const HOME: Header = {
     title: 'articles.last',
     subtitle: 'articles.description',
-    bg: './assets/img/home-bg.jpg'
+    bg: 'https://mugan86.com/serviraces/web/assets/img/background.jpg'
+    /*bg: './assets/img/home-bg.jpg'*/
 };
 
-export const LOGIN: Header =
-{
+export const LOGIN: Header = {
     title: 'login.title',
     subtitle: 'login.description',
     bg: './assets/img/home-bg.jpg'
 };
 
-export const REGISTER: Header =
-{
+export const REGISTER: Header = {
     title: 'register.title',
     subtitle: 'register.description',
     bg: './assets/img/home-bg.jpg'
 };
 
-export const ABOUT: Header =
-{
+export const ABOUT: Header = {
     title: 'about.title',
     subtitle: 'about.description',
     bg: './assets/img/home-bg.jpg'
 };
 
-export const COURSES: Header =
-{
+export const COURSES: Header = {
     title: 'courses.title',
     subtitle: 'courses.description',
     bg: './assets/img/home-bg.jpg'
 };
 
-export const CONTACT: Header =
-{
+export const CONTACT: Header = {
     title: 'contact.title',
     subtitle: 'contact.description',
     bg: './assets/img/contact-bg.jpg'
 };
 
-export const PORTFOLIO: Header =
-{
+export const PORTFOLIO: Header = {
     title: 'portfolio.title',
     subtitle: 'portfolio.description',
     bg: './assets/img/portfolio-bg.jpg'
